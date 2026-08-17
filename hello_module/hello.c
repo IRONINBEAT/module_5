2f7d2987f84f7d91009fb134866483d9d285fb00
Ilya Voronin <ilyavoron2004@gmail.com>
2026-08-17 21:42:20 +0700

    Init commit

---
+++ hello_module/hello.c
@@ -0,0 +1,22 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init hello_init(void)
+{
+	printk(KERN_INFO "Hello, World! Module loaded by Ilya Voronin.\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	printk(KERN_INFO "Goodbye, World! Module unloaded.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("VoroninLicense");
+MODULE_AUTHOR("Ilya Voronin <ilyavoron2004@gmail.com>");
+MODULE_DESCRIPTION("Учебный модуль ядра Hello World для практики модуля 5");
+MODULE_VERSION("1.0");
