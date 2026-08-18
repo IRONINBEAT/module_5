68e718690087f37a9b9a037d256d3095c0d7b206
Ilya Voronin <ilyavoron2004@gmail.com>
2026-08-18 20:55:55 +0700

    licences change

--- hello_module/hello.c
+++ hello_module/hello.c
@@ -2,6 +2,11 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+MODULE_LICENSE("VoroninIlyaLicense");
+MODULE_AUTHOR("Ilya Voronin <ilyavoron2004@gmail.com>");
+MODULE_DESCRIPTION("Учебный модуль ядра Hello World для практики #1 модуля 5");
+MODULE_VERSION("1.0");
+
 static int __init hello_init(void)
 {
 	printk(KERN_INFO "Hello, World! Module loaded by Ilya Voronin.\n");
@@ -15,8 +20,3 @@ static void __exit hello_exit(void)
 
 module_init(hello_init);
 module_exit(hello_exit);
-
-MODULE_LICENSE("VoroninLicense");
-MODULE_AUTHOR("Ilya Voronin <ilyavoron2004@gmail.com>");
-MODULE_DESCRIPTION("Учебный модуль ядра Hello World для практики модуля 5");
-MODULE_VERSION("1.0");
